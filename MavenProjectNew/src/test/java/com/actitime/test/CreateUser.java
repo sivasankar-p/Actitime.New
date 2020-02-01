1c502f3b80347970c7adc1eadcb5cb0f1a2141fa
sp205 <sp205@IN5CD6411J9Y.CSCMWS.CSCMWS.COM>
2020-02-01 13:04:18 +0530

    first

---
+++ MavenProjectNew/src/test/java/com/actitime/test/CreateUser.java
@@ -0,0 +1,14 @@
+package com.actitime.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateUser {
+	@Test
+	public void testCreateUser()
+	{
+		Reporter.log("CreateUser",true);
+		
+	}
+	
+}
